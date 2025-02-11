6ed378d8adfd5f09b29797370327942c5fce7956
zjhM3l <zhangjiahe24@outlook.com>
2025-02-11 01:30:59 -0600

    user end show cart items

--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,12 +1,17 @@
 package com.sky.controller.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aliyuncs.ecs.model.v20140526.AttachKeyPairResponse;
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 
@@ -34,5 +39,16 @@ public class ShoppingCartController {
     public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
         shoppingCartService.addShoppingCart(shoppingCartDTO);
         return Result.success();
+    }
+
+    /**
+     * 查看购物车
+     * @return 
+     */
+    @GetMapping("/list")
+    @ApiOperation("查看购物车")
+    public Result<List<ShoppingCart>> list() {
+        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
+        return Result.success(list);
     }
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
+import java.util.List;
+
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 
 public interface ShoppingCartService {
 
@@ -9,4 +12,10 @@ public interface ShoppingCartService {
      * @param shoppingCartDTO
      */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     * @return 
+     */
+    List<ShoppingCart> showShoppingCart();
 }
