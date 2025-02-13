cf0dae903ccf42f7bfac3716faf107cecee92574
zjhM3l <zhangjiahe24@outlook.com>
2025-02-12 21:41:56 -0600

    new order notify debug(every 5min voice call) & order reminder also by websocket done

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -556,4 +556,28 @@ public class OrderServiceImpl implements OrderService{
 
         orderMapper.update(orders);
     }
+
+    /**
+     * 客户催单
+     *
+     * @param id
+     */
+    public void reminder(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        // 通过Websocket向客户端浏览器推送消息
+        Map map = new HashMap();
+        map.put("type", 2); // 1表示来单提醒，2表示客户催单
+        map.put("orderId", id);
+        map.put("content", "订单号：" + ordersDB.getNumber() + "，请尽快处理");
+        String json = JSON.toJSONString(map);
+        
+        webSocketServer.sendToAllClient(json);
+    }
 }
--- sky-take-out/sky-server/src/main/java/com/sky/task/WebSocketTask.java
+++ sky-take-out/sky-server/src/main/java/com/sky/task/WebSocketTask.java
@@ -1,10 +1,6 @@
 package com.sky.task;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.sky.websocket.WebSocketServer;
@@ -17,8 +13,9 @@ public class WebSocketTask {
     /**
      * 通过WebSocket每隔5秒向客户端发送消息
      */
-    @Scheduled(cron = "0/5 * * * * ?")
-    public void sendMessageToClient() {
-        webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
-    }
+    // 为了不影响主程序催单和接单提醒，先注释掉了，不然会一直语音播报
+    // @Scheduled(cron = "0/5 * * * * ?")
+    // public void sendMessageToClient() {
+    //     webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
+    // }
 }
